3d1374005addaa47101e8ee63c77060c082a2e4f
Efendo <efe_mustafa.oener@icloud.com>
2023-05-31 18:32:36 +0200

    more_animations

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 class Cbutton:
-      	def __init__(self):
+      def __init__(self):
             self.ScoreDisp = Element("ScoreDisp")
             self.score = 0
 
-      	def click(self):
+      def click(self):
+            self.ScoreDisp.remove_class("transition-all")
+            self.ScoreDisp.remove_class("duration-150")
+            self.ScoreDisp.remove_class("rotate-[360deg]")
             self.score += 1
             self.ScoreDisp.element.innerHTML = self.score
+
+            if self.score % 100 == 0:
+                  self.ScoreDisp.add_class("rotate-[360deg]")
+                  self.ScoreDisp.add_class("transition-all")
+                  self.ScoreDisp.add_class("duration-150")
 
 
 C = Cbutton()
